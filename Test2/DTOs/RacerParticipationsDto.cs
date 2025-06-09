26033cc9c1420755bb8cdef9f713f351171199a6
Alexandr Biktimirov <alex.biktimirov38@gmail.com>
2025-06-09 18:55:10 +0200

    Add constrains for RacerParticipationsDto.cs

--- Test2/DTOs/RacerParticipationsDto.cs
+++ Test2/DTOs/RacerParticipationsDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Test2.DTOs;
 
 public class RacerParticipationsDto
 {
+    [Required]
+    [MaxLength(50)]
     public string RaceName { get; set; } = null!;
+    [Required]
+    [MaxLength(100)]
     public string TrackName { get; set; } = null!;
+    [Required]
     public List<ParticipationsCreateDto> Participations { get; set; } = null!;
 }
 
