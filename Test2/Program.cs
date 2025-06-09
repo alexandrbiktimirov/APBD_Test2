082ed408cf149d7ea9ca814585e85327e8053665
Alexandr Biktimirov <alex.biktimirov38@gmail.com>
2025-06-09 18:38:19 +0200

    Initial commit

---
+++ Test2/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Test2.Data;
+using Test2.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<IDbService, DbService>();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
